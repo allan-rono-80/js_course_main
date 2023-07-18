3f31cd5ce9d5d5627caafc74b289d1adf4b80861
Allan Rono <allanrono80@gmail.com>
2023-07-18 20:22:47 +0300

    js4 Numbers, js5 Math Methods.

---
+++ js4/js/main.js
@@ -0,0 +1,14 @@
+// Numbers
+
+// An integer is a whole number.
+const myNumber = 42;
+
+// A number with a decimal point is a float
+const myFloat = 42.01;
+console.log(myFloat);
+
+console.log(myNumber);
+
+// We can change a string data type to a number datatype by using a number function.
+
+// Always remember that 0 = false in JavaScript.
---
+++ js5/js/main.js
@@ -0,0 +1,7 @@
+// Math Methods and Properties.
+
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
