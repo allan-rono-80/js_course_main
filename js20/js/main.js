17dd8942f4ab96ae807e1f44d6ab3e1bda9231e6
Allan Rono <allanrono80@gmail.com>
2023-08-17 21:16:31 +0300

    Handling errors

---
+++ js20/js/main.js
@@ -0,0 +1,35 @@
+// JavaScript Errors and Error Handling
+
+"use strict";
+
+const makeError =
+
+let i = 1;
+
+while (i <= 5) {
+
+try {
+
+if (i % 2 !== 0) {
+
+throw new Error("Odd number!");
+
+}
+
+console.log("Even number!") } catch (err) {
+
+console.error(err.stack);
+
+finally {
+
+console.log("...finally");
+
+i++;
+
+}
+
+}
+
+};
+
+makeError();
