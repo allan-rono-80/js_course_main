84d8fcb5f73719959825cde51538667d47c6e168
Allan Rono <allanrono80@gmail.com>
2023-08-17 19:23:48 +0300

    Javascript Classes

---
+++ js18/js/main.js
@@ -0,0 +1,34 @@
+// Javascript Classes
+
+class Pizza {
+
+    constructor(pizzaSize) {
+    
+    this.size = pizzaSize; this.crust = "original";
+    
+     }
+    
+    getCrust() { return this.crust;
+    
+     }
+    
+    setCrust (pizzaCrust) {
+    
+    this.crust pizzaCrust;
+    
+    }
+    
+     }
+    
+    class SpecialtyPizza extends Pizza { constructor(pizzaSize){
+    
+    super(pizzaSize); this.type = "The Works";
+    
+    }
+    
+    slice() {
+    
+    console.log('Our ${this.type} ${pizza')
+    }
+
+    }
