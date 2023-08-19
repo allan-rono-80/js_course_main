98d97f66d05e08fe4fae4753b509ea568fdc092b
Allan Rono <allanrono80@gmail.com>
2023-08-19 22:52:44 +0300

    Web storage

---
+++ js23/js/main.js
@@ -0,0 +1,26 @@
+ // Web Storage AP1
+
+// Not part of the DOM refers to the Window API 
+// Available to JS via the global variable: window
+
+//We do not have to type window. It is implied:
+
+const myArray["eat", "sleep", "code"];
+
+const myObject = {
+
+{
+
+name: "Dave",
+
+hobbies: ["eat", "sleep", "code"],
+
+logName: function () { console.log(this.name);
+
+};
+
+localStorage.setItem("myLocalStore", JSON.stringify(myArray)); const storeLength localStorage.length;
+
+const myLocalData= JSON.parse(localStorage.getItem("myLocalStore"));
+
+console.logstoreLength};
